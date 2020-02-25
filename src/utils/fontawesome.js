7967f53fbb6cc214e8b454714710e923e6bdad81
ribeiroyoann <yoribeir@student.42.fr>
2020-02-25 18:40:03 +0100

    center

--- src/utils/fontawesome.js
+++ src/utils/fontawesome.js
@@ -1,4 +1,3 @@
 import { library } from "@fortawesome/fontawesome-svg-core"
-import { faCoffee } from "@fortawesome/free-solid-svg-icons"
 import { fab } from "@fortawesome/free-brands-svg-icons"
 library.add(fab)
